be21770c8c931f4f85a3095fa64b100f8e9aa07b
Stacy Vlasits <stacyvlasits@austin.utexas.edu>
2023-02-23 13:03:43 -0600

    Fix wp_debug irritants
    Enable all wp_debug options to be set by env variables; set good dev defaults

--- versions/6-php7.4/wp-config-docker.php
+++ versions/6-php7.4/wp-config-docker.php
@@ -110,7 +110,9 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', false) );
+define( 'WP_DEBUG_LOG', !!getenv_docker('WORDPRESS_DEBUG_LOG', false) );
+define( 'WP_DEBUG_DISPLAY', !!getenv_docker('WORDPRESS_DEBUG_DISPLAY', false) );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
